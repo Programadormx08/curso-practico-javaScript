9b2c45dee55d50bc97dbd6e778d6418e8b2b2ca0
Orlando Leyva <leyvaorlando316@gmail.com>
2022-01-23 10:19:28 -0700

    calculo de figuras geometricas, version inicial

--- figuras/codigo.js
+++ figuras/codigo.js
@@ -1,6 +1,42 @@
-// const perimetroCuadrado = (lado)=>
-// {
-//     return resultado = lado * 4;
-// };
+const cuadrado = document.querySelector(".img1");
+const triangulo = document.querySelector(".img2");
+const circulo = document.querySelector(".img3");
 
-// console.log(perimetroCuadrado(parseInt(prompt("cuanto mide un lado del cuadrado"))));
+cuadrado.addEventListener("click", ()=>
+{
+    let dato = parseInt(prompt("¿cuanto mide cada lado de cuadrado?"));
+    let perimetro = dato * 4;
+    let area = dato * dato;
+    let codeHtml = `<p>Figura: cuadrado</p><br>
+                    <p>Perimetro: ${perimetro}</p><br>
+                    <p>Area: ${area}</p>`
+    document.querySelector(".resultado").innerHTML = codeHtml;
+});
+
+triangulo.addEventListener("click", ()=>
+{
+    let base = parseInt(prompt("¿cuanto mide la base del triangulo?"));
+    let lado = parseInt(prompt("¿cuanto mide cada lado del triangulo?"));
+    let altura = parseInt(prompt("¿cuanto mide la altura del triangulo?"));
+    let perimetro = base + (lado * 2);
+    let area = (base * altura)/2;
+
+    let codeHtml = `<p>Figura: Triangulo</p><br>
+                    <p>Perimetro: ${perimetro}</p><br>
+                    <p>Area: ${area}</p>`
+    document.querySelector(".resultado").innerHTML = codeHtml;
+});
+
+circulo.addEventListener("click", ()=>
+{
+    let diametro = parseInt(prompt("¿cuanto mide el diametro del circulo?"));
+    let radio = diametro / 2;
+    let area = 3.1416 * (radio * radio);
+
+    let codeHtml = `<p>Figura: Circulo</p><br>
+                    <p>Radio: ${radio}</p><br>
+                    <p>Diametro: ${diametro}</p><br>
+                    <p>Area: ${area}</p>`
+    document.querySelector(".resultado").innerHTML = codeHtml;
+});
+
