1d0aa674524158a70fd105c3df8ec5eaca8a5a16
Orlando Leyva <leyvaorlando316@gmail.com>
2022-01-24 14:55:49 -0700

    version 1.0

--- porcentajes/codigo.js
+++ porcentajes/codigo.js
@@ -5,6 +5,8 @@ document.querySelector(".enviar").addEventListener("click", ()=>
 {
     let descuentoGenerado = (parseInt(descuento.value) * parseInt(precio.value))/100;
     let pago = parseInt(precio.value) - descuentoGenerado;
+    if (isNaN(pago)) {
+        document.querySelector(".pago").value = "ingrese solo números";
+    } else document.querySelector(".pago").value = pago;
     
-    document.querySelector(".pago").value = pago;
 })
