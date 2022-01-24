f5cee2a1662efe15319946c140563219969d2e72
Orlando Leyva <leyvaorlando316@gmail.com>
2022-01-24 14:47:13 -0700

    cree el codigo que genera el descuento

--- porcentajes/codigo.js
+++ porcentajes/codigo.js
@@ -1,4 +1,10 @@
-// const precio = document.querySelector(".precio");
-// const descuento = document.querySelector(".desuento");
+const precio = document.querySelector(".precio");
+const descuento = document.querySelector(".descuento");
 
-// document.query
+document.querySelector(".enviar").addEventListener("click", ()=>
+{
+    let descuentoGenerado = (parseInt(descuento.value) * parseInt(precio.value))/100;
+    let pago = parseInt(precio.value) - descuentoGenerado;
+    
+    document.querySelector(".pago").value = pago;
+})
