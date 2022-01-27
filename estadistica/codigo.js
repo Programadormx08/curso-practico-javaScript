e51ad965e351781cd70da56dfc8e32aa069d59e8
Orlando Leyva <leyvaorlando316@gmail.com>
2022-01-26 20:03:42 -0700

    mediana code ready

--- estadistica/codigo.js
+++ estadistica/codigo.js
@@ -1,5 +1,6 @@
-let numeros = [1,3,7,1,9,3,12];
-let numbers2 = [3,46,12,89,120,54];
+let numeros = [1,3,7,1,9,3];
+let numbers2 = [12,32,56,87,132,45,23];
+let numbers3 = [1,34,45,87,98,87,34,45,87,90,2,45,67,89,87,9]
 
 
 // const promedio = ()=>
@@ -13,12 +14,31 @@ let numbers2 = [3,46,12,89,120,54];
 
 // console.log(promedio());
 
-const mediana= ()=>
+const compare = (a,b)=>
 {
-    let half = numbers2.length / 2 % 2;
-    if (half == 0) console.log("even list")
-    else console.log("odd list");
+    return a-b;
+}
+
+const mediana = ()=>
+{
+    let half = numbers2.length % 2;
+    if (half == 0) {
+        numbers2.sort(compare);
+        let halfNumbers = numbers2.length / 2;
+        let calculation = (numbers2[halfNumbers -1] + numbers2[halfNumbers]) / 2;
+        console.log(numbers2);
+        console.log(calculation);  
+    } else
+    {
+        numbers2.sort(compare);
+        let halfNumbers = (numbers2.length -1) / 2
+        let result = numbers2[halfNumbers];
+        console.log(numbers2);  
+        console.log(result);
+    }
 
 };
 
 mediana();
+
+//numero de elementos menos uno entre 2: esa es la mediana.
